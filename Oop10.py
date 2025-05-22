b8fb27c4dc73b7aaf9b4c67e351b489968788c8a
furqunali <furqanali2628@gmail.com>
2025-05-22 07:21:16 +0500

    Add files via upload

---
+++ Oop10.py
@@ -0,0 +1,22 @@
+# Multiple class with Super Method (Super method is used to access methods of the parent class)
+
+class Car:
+    def __init__(self, type):
+        self.type = type
+
+    @staticmethod
+    def start():
+        print("car started..")
+
+    @staticmethod
+    def stop():
+        print("car stopped.")
+
+class ToyotaCar(Car):
+    def __init__(self, name, type):
+        super().__init__(type)
+        self.name = name
+        super().start()
+
+car1 = ToyotaCar("Corola", "hybride")
+print(car1.type)
